3c893538b6ce2207989418b6032680a0f9e3cc8c
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2017-05-16 23:58:07 -0300

    Commit

---
+++ db_versions/db-198-20170516-221834-3.postgresql.sql
@@ -0,0 +1,3 @@
+--liquibase formatted sql
+--changeset carloseduardo:1494983926647-1
+INSERT INTO public.db_version(versao_major, versao_minor, build, compilation) VALUES ('GestorLotericoWeb', '1.0-SNAPSHOT', '198','20170516-221834'); 
